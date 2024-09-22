c4ac588b92c3597d8ae10c46a12c1c24dfdd6dcb
HarveyChang <harvey@ptc-nec.com.tw>
2024-09-22 11:10:32 +0800

    modify for GCE

--- GCE/main.tf
+++ GCE/main.tf
@@ -28,6 +28,8 @@ resource "google_compute_instance" "docker_vm" {
 
   metadata_startup_script = <<-EOF
     #! /bin/bash
+    sudo apt update && sudo apt install git -y
+    
     sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
     curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
     sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
